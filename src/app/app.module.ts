18dbc8b3530b4129e028ae32df8e6e23395fee2a
wfpaisa <wfpaisa@gmail.com>
2022-05-30 08:57:04 -0500

    📦 NEW: Home base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { ButtonComponent } from './components/form/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/form/button/button.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+  @Input() color: string = ''
+  @Input() text: string = ''
+  @Output() btnClick = new EventEmitter
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onClick(text: string) {
+    this.btnClick.emit(text)
+  }
+
+}
